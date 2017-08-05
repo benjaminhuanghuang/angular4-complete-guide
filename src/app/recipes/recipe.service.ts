875682f1a89518751867816b2be2d0d93e1a584c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-05 15:10:53 -0700

    add ingredient to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,17 +2,29 @@ import { EventEmitter } from '@angular/core';
 
 //
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
-export class RecipeService{
+export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'asdfdsa', 'https://cdn.vox-cdn.com/thumbor/WOgDnXAL2nI4upgY-j8TkV-hrD0=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/56029737/akrales_170118_1403_A_0018.0.0.jpg'),
-    new Recipe('Test Recipe 2', 'asdfdsa', 'https://cdn.vox-cdn.com/thumbor/WOgDnXAL2nI4upgY-j8TkV-hrD0=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/56029737/akrales_170118_1403_A_0018.0.0.jpg')
+    new Recipe('Test Recipe', 'asdfdsa',
+      'https://cdn.vox-cdn.com/thumbor/WOgDnXAL2nI4upgY-j8TkV-hrD0=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/56029737/akrales_170118_1403_A_0018.0.0.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fish', 20)
+
+      ]),
+    new Recipe('Test Recipe 2', 'asdfdsa',
+      'https://cdn.vox-cdn.com/thumbor/WOgDnXAL2nI4upgY-j8TkV-hrD0=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/56029737/akrales_170118_1403_A_0018.0.0.jpg',
+      [
+        new Ingredient('Buns', 1),
+        new Ingredient('Maat', 20)
+      ])
   ];
 
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();  //return a copy
   }
 }
